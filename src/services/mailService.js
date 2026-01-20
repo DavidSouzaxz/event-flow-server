4a630acfe5e25949a529601d74d147962fcd54aa
DavidSouzaxz <daviddavi1221@gmail.com>
2026-01-20 11:36:21 -0300

    Fix: ajusted por push emails host

--- src/services/mailService.js
+++ src/services/mailService.js
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "smtp.gmail.com",
+  host: "smtp.gmail.com",
   port: 587,
   secure: false,
   auth: {
